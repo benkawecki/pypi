5ec9b445921d6e8401df778cfd1b049fca6b1ee4
Ben Kawecki <bkbkawecki@gmail.com>
2019-01-15 21:50:35 -0500

    error configuration, returns an config confliction while the config is trying to commit

--- pypi/__init__.py
+++ pypi/__init__.py
@@ -5,7 +5,20 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     with Configurator(settings=settings) as config:
-        config.include('pyramid_chameleon')
-        config.include('.routes')
-        config.scan()
+
+        config.include(init_routing)
+        print("hello")
+        config.include("pyramid_chameleon")
+
     return config.make_wsgi_app()
+
+
+def init_includes(config):
+    config.include("pyramid_chameleon")
+
+
+def init_routing(config):
+    config.add_static_view("static", "static", cache_max_age=3600)
+    config.add_route("home", "/")
+    config.add_route("about", "/about")
+    config.scan()
